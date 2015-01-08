7ba4feff62ef4de3ba8018471ea69d961e121234
Shuai Yuan <jeff.yuans@gmail.com>
2015-01-07 23:38:16 -0800

    Some refactor.

--- client.js
+++ client.js
@@ -1,24 +1,22 @@
-	
-
-
-var shouldNotify = true;
-var socket = io.connect('http://localhost:8080');
+    var shouldNotify = true;
+    var socket = io.connect('http://localhost:8080');
 	var UNIVERSE = 100000;
 	var rid = Math.floor(Math.random() * UNIVERSE);
 	socket.on('notification', function (data) {
 		console.error('hello notification');
 	    var msg = JSON.parse(data.message);
 	    console.error(msg);
-	    if (msg.rid === rid) {
+	    if (msg.rid == rid) {
 	    	return;
 	    }
 	    shouldNotify = false;
 	    switch(msg.operator) {
 	    case 'play':
-	    	playVideo();
+	    	console.error('told to play');
+            playVideo();
 	    	break;
 	    case 'pause':
-	    	console.error('pausing');
+	    	console.error('told to pause');
 	    	pauseVideo();
 	    	break;
 	    }
@@ -32,41 +30,6 @@ var socket = io.connect('http://localhost:8080');
     	}
 		
 		$.post('http://localhost:8080/?operator=' + operator + '&rid=' + rid, function(a) {}); 
-		//The url we want is `www.nodejitsu.com:1337/`
-//			var options = {
-//			  host: '127.0.0.1',
-//			  path: '/',
-//			  //since we are listening on a custom port, we need to specify it by hand
-//			  port: '8080',
-//			  //This is what changes the request to a POST request
-//			  method: 'GET',
-//			  qs: {message: 'shuo'}
-//			};
-//			
-//			
-//			// http://stackoverflow.com/questions/9577611/http-get-request-in-node-js-express
-//		    var req = http.request(options, function(res)
-//		    {
-////		        var output = '';
-////		        console.log(options.host + ':' + res.statusCode);
-////		        res.setEncoding('utf8');
-////
-////		        res.on('data', function (chunk) {
-////		            output += chunk;
-////		        });
-////
-////		        res.on('end', function() {
-////		            var obj = JSON.parse(output);
-////		            onResult(res.statusCode, obj);
-////		        });
-//		    });
-//
-//		    req.on('error', function(err) {
-//		        //res.send('error: ' + err.message);
-//		    });
-//
-//		    req.end();
-//	
 	}
 	
 	
@@ -82,26 +45,25 @@ var socket = io.connect('http://localhost:8080');
     var player;
    
     function onYouTubeIframeAPIReady() {
-      player = new YT.Player('player', {
-        height: '390',
-        width: '640',
-        videoId: 'M7lc1UVf-VE',
-        events: {
-          'onReady': onPlayerReady,
-          'onStateChange': onPlayerStateChange
-        }
-      });
+        player = new YT.Player('player', {
+            height: '390',
+            width: '640',
+            videoId: 'M7lc1UVf-VE',
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+        });
     }
 
     // 4. The API will call this function when the video player is ready.
     function onPlayerReady(event) {
-      event.target.playVideo();
+        //event.target.playVideo();
     }
 
     // 5. The API calls this function when the player's state changes.
     //    The function indicates that when playing a video (state=1),
     //    the player should play for six seconds and then stop.
-    var done = false;
     function onPlayerStateChange(event) {
     	
     	console.error(event);
@@ -117,8 +79,8 @@ var socket = io.connect('http://localhost:8080');
     	
     	switch(event.data) {
     	case 2:
-    	   pauseVideo();
-    	   break;
+            pauseVideo();
+            break;
     	case 1:
     		playVideo();
     		break;
@@ -129,12 +91,12 @@ var socket = io.connect('http://localhost:8080');
     function pauseVideo() {
     	console.error('pausing');
     	player.pauseVideo();
-    		notifyServer('pause');
+        notifyServer('pause');
     }
     
     function stopVideo() {
-      player.stopVideo();
-      notifyServer('stop');
+        player.stopVideo();
+        notifyServer('stop');
     }
     
     function playVideo() {
